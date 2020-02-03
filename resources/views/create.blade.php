27679497af3ffa165194f294977aeb378d31582f
viktor-poberezhniy <viktor.poberezhniy@gmail.com>
2020-02-04 01:17:40 +0200

    1) Added some notes to task.txt file 2) Created a new view home.blade.php 3) Reconfigured the route from welcome to home 4) Added a layouts \ app.blade.php view 5) Commented out Authorization and Registration in the layouts \ app.blade.php file 6) Added Public \ CSS and Public \ JS files, and also created the Public \ Upload folder to upload files 7) Created a file create.blade.php (for the form)

---
+++ resources/views/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Create page
+@endsection
+
+@section('content')
+    <h1>Some head</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title')
+    form page
+@endsection
+
+@section('content')
+
+
+
+
+
+
+
+@endsection
